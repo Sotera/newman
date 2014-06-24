bf9f371b2326ee743e13e0b55f39f5f93071d763
Scott Haleen <scott.haleen@gmail.com>
2014-06-24 09:23:06 -0400

    changed enrichments to insert into instead of reading and inserting through python

--- src/enrich_email_comms.py
+++ src/enrich_email_comms.py
@@ -5,11 +5,12 @@ import argparse, sys
 
 sys.path.append("./demail")
 from newman.db.domain import Tx, Fact
-from newman.db.mysql import execute_query
+from newman.db.mysql import execute_query, execute_nonquery
 from newman.db.newman_db import newman_connector
 
-stmt = (
-    " select distinct t1.obj, t2.predicate, t2.obj "
+stmt_sent_to = (
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select distinct t1.obj, 'email_addr', t2.predicate, t2.obj, %s "
     " from facts t1 join facts t2 " 
     " ON t1.subject = t2.subject and t1.schema_name = t2.schema_name "
     " where t1.schema_name = 'email' "
@@ -18,7 +19,8 @@ stmt = (
 )
 
 stmt_total_recipients = (
-    " select t1.obj, count(t2.obj) "
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select t1.obj, 'email_addr', 'total_recipients', count(t2.obj), %s "
     " from facts t1 join facts t2 " 
     " ON t1.subject = t2.subject and t1.schema_name = t2.schema_name "
     " where t1.schema_name = 'email' "
@@ -28,7 +30,8 @@ stmt_total_recipients = (
 )
 
 stmt_total_received = (
-    " select obj, count(1) "
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select obj, 'email_addr', 'total_received', count(1), %s "
     " from facts "
     " where schema_name = 'email' " 
     " and predicate in ('to', 'cc', 'bcc') "
@@ -36,7 +39,8 @@ stmt_total_received = (
 )
 
 stmt_total_sent = (
-    " select obj, count(1) "
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select obj, 'email_addr', 'total_sent', count(1), %s "
     " from facts "
     " where schema_name = 'email' " 
     " and predicate in ('from') "
@@ -44,7 +48,8 @@ stmt_total_sent = (
 )
 
 stmt_email_sent_time = (
-    " select t1.subject, t1.obj as email, t2.obj as dt "
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select  t1.obj, 'email_addr', 'sent_time', t2.obj, %s "
     " from facts t1 join facts t2 "
     " ON t1.subject = t2.subject and t1.schema_name = t2.schema_name "
     " where t1.schema_name = 'email' "
@@ -53,18 +58,14 @@ stmt_email_sent_time = (
 )
 
 stmt_email_received_time = (
-    " select t1.subject, t1.obj as email, t2.obj as dt "
+    " insert into facts (subject, schema_name, predicate, obj, tx) "
+    " select t1.obj, 'email_addr', 'received_time', t2.obj as dt, %s "
     " from facts t1 join facts t2 "
     " ON t1.subject = t2.subject and t1.schema_name = t2.schema_name "
     " where t1.schema_name = 'email' "
     " and t1.predicate in ('to', 'cc', 'bcc') "
     " and t2.predicate = 'datetime' "
 )
-
-
-
-#TODO most of these can be changed to insert into statements instead
-#of reading the results to python and writing back to the database
 
 if __name__ == "__main__":
 
@@ -76,54 +77,43 @@ if __name__ == "__main__":
         print "tx: %s" % txid
         print "enrich sent to recipient communications"
         facts = Fact(write_cnx.conn(), autocommit=False)
-        with execute_query(read_cnx.conn(), stmt) as qry:
-            for from_, pred, to in qry.cursor():
-                facts.addFact(from_, "email_addr", pred, to, txid) 
-            
+        
+        with execute_nonquery(write_cnx.conn(), stmt_sent_to, txid) as qry:
+            pass
         write_cnx.commit()
     
         txid = Tx(read_cnx.conn()).next()
         print "tx: %s" % txid
         print "enrich total sent to " 
-        with execute_query(read_cnx.conn(), stmt_total_recipients) as qry:
-            for from_, count in qry.cursor():
-                facts.addFact(from_, "email_addr", "total_recipients", count, txid)
-
+        with execute_query(write_cnx.conn(), stmt_total_recipients, txid) as qry:
+            pass
         write_cnx.commit()
     
         txid = Tx(read_cnx.conn()).next()
         print "tx: %s" % txid
         print "enrich total received "
-        with execute_query(read_cnx.conn(), stmt_total_received) as qry:
-            for email_addr, count in qry.cursor():
-                facts.addFact(email_addr, "email_addr", "total_received", count, txid)
-
+        with execute_query(write_cnx.conn(), stmt_total_received, txid) as qry:
+            pass
         write_cnx.commit()
 
 
         txid = Tx(read_cnx.conn()).next()
         print "tx: %s" % txid
         print "enrich total sent "
-        with execute_query(read_cnx.conn(), stmt_total_sent) as qry:
-            for email_addr, count in qry.cursor():
-                facts.addFact(email_addr, "email_addr", "total_sent", count, txid)
-
+        with execute_query(read_cnx.conn(), stmt_total_sent, txid) as qry:
+            pass
         write_cnx.commit()
 
         txid = Tx(read_cnx.conn()).next()
         print "tx: %s" % txid
         print "enrich sent times "
-        with execute_query(read_cnx.conn(), stmt_email_sent_time) as qry:
-            for email_id, from_, dt in qry.cursor():
-                facts.addFact(from_, "email_addr", "sent_time", dt, txid)
-
+        with execute_query(read_cnx.conn(), stmt_email_sent_time, txid) as qry:
+            pass
         write_cnx.commit()        
         
         txid = Tx(read_cnx.conn()).next()
         print "tx: %s" % txid
         print "enrich received times "
-        with execute_query(read_cnx.conn(), stmt_email_received_time) as qry:
-            for email_id, email_addr, dt in qry.cursor():
-                facts.addFact(email_addr, "email_addr", "received_time", dt, txid)
-
+        with execute_query(read_cnx.conn(), stmt_email_received_time, txid) as qry:
+            pass
         write_cnx.commit()        
