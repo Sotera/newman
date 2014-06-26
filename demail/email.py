f7457aa7a09055c39ceb281099fe859af3cf88a2
Scott Haleen <scott.haleen@gmail.com>
2014-06-26 13:02:27 -0400

    email service

---
+++ demail/email.py
@@ -0,0 +1,40 @@
+from newman.db.newman_db import newman_connector
+from newman.db.mysql import execute_query, execute_nonquery
+from newman.utils.functions import nth
+
+import tangelo
+import cherrypy
+import json
+import urllib
+
+
+
+stmt_email_by_id = (
+    " select id, dir, datetime, from_addr, tos, ccs, bccs, subject, body, attach "
+    " from email "
+    " where id = %s "
+)
+
+#GET /email/<id>
+def getEmail(*args):
+    email=urllib.unquote(nth(args, 0, ''))
+    if not email:
+        return tangelo.HTTPStatusCode(400, "invalid service call - missing id")
+    
+    with newman_connector() as read_cnx:
+        with execute_query(read_cnx.conn(), stmt_email_by_id, email) as qry:
+            tangelo.log("node-vals: %s" % qry.stmt)
+            rtn = qry.cursor().fetchone()
+            tangelo.content_type("application/json")
+            return rtn if rtn else []
+
+actions = {
+    "email": getEmail
+}
+
+def unknown(*args):
+    return tangelo.HTTPStatusCode(400, "invalid service call")
+
+@tangelo.restful
+def get(action, *args, **kwargs):
+    return actions.get(action, unknown)(*args)
