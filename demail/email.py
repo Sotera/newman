9acab6b7dc29bbd25d9e7e2ff2560cb4577378e6
Scott Haleen <scott.haleen@gmail.com>
2014-06-29 16:34:09 -0400

    updated email body service

--- demail/email.py
+++ demail/email.py
@@ -8,20 +8,19 @@ import json
 import urllib
 
 
-
 stmt_email_by_id = (
     " select id, dir, datetime, from_addr, tos, ccs, bccs, subject, body, attach "
     " from email "
     " where id = %s "
 )
 
+stmt_email_entities_by_id = (
+    " select subject, entity_type, idx, value "
+    " from entity "
+    " where email_id = %s "
+)
 
-#GET /email/<id>
-def getEmail(*args):
-    email=urllib.unquote(nth(args, 0, ''))
-    if not email:
-        return tangelo.HTTPStatusCode(400, "invalid service call - missing id")
-    
+def queryEmail(email):
     with newman_connector() as read_cnx:
         with execute_query(read_cnx.conn(), stmt_email_by_id, email) as qry:
             tangelo.log("node-vals: %s" % qry.stmt)
@@ -29,8 +28,36 @@ def getEmail(*args):
             tangelo.content_type("application/json")
             return rtn if rtn else []
 
+def queryEntity(email):
+    with newman_connector() as read_cnx:
+        with execute_query(read_cnx.conn(), stmt_email_entities_by_id, email) as qry:
+            tangelo.log("node-vals: %s" % qry.stmt)
+            rtn = [r for r in qry.cursor()]
+
+            return rtn if rtn else []
+
+#GET /email/<id>
+def getEmail(*args):
+    email=urllib.unquote(nth(args, 0, ''))
+    if not email:
+        return tangelo.HTTPStatusCode(400, "invalid service call - missing id")
+    
+    tangelo.content_type("application/json")    
+    return { "email" : queryEmail(email), "entities": queryEntity(email) }
+
+#GET /entities/<id>
+def getEntities(*args):
+    email=urllib.unquote(nth(args, 0, ''))
+    if not email:
+        return tangelo.HTTPStatusCode(400, "invalid service call - missing id")
+    tangelo.content_type("application/json")    
+    return queryEntity(email)
+
+    
+
 actions = {
-    "email": getEmail
+    "email": getEmail,
+    "entities" : getEntities
 }
 
 def unknown(*args):
