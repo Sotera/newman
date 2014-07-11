741ff329fe5c69fa78ba2a57db0353b3d3a88488
Scott Haleen <scott.haleen@gmail.com>
2014-07-11 13:50:17 -0400

    topic clustering run script

--- bin/run_topic_clustring.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python /srv/software/topic-clustering/topic/run_all.py -num_topics 20 -r email_ingester data/walker/output.csv tmp/
---
+++ topic/run_topic_clustring.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e 
+
+RUN_DIR=$(pwd)
+TOPIC_DIR=/srv/software/topic-clustering/topic
+
+printf "working dir $RUN_DIR\n"
+printf "topic-clustering dir $TOPIC_DIR\n"
+
+if [ -e $TOPIC_DIR/Ingest/email_ingester.py ]; then
+    rm -f $TOPIC_DIR/Ingest/email_ingester.py
+fi
+
+cp topic/email_ingester.py $TOPIC_DIR/Ingest/
+
+cd $TOPIC_DIR
+
+if [ -d tmp-newman/ ]; then
+    rm -rf tmp-newman/
+fi
+
+mkdir tmp-newman
+
+#needs to match the number of topics
+NUM_TOPIC=20
+
+python run_all.py -num_topics $NUM_TOPIC -r email_ingester $RUN_DIR/data/walker/output.csv tmp/
+
+cd $RUN_DIR
+
+if [ -e tmp/topic_cluster_scores.dat ]; then
+    rm -rf tmp/topic_cluster_scores.dat
+fi
+
+paste <(cat $TOPIC_DIR/tmp/output.csv.$NUM_TOPIC.counts.txt | awk '{ print $1 }') <(sed 's/^[ ]*//' $TOPIC_DIR/tmp/output.csv.$NUM_TOPIC.d2z.txt) > tmp/topic_cluster_scores.dat
+
+if [ -e tmp/topic_cluster.idx ]; then
+    rm -rf tmp/topic_cluster.idx
+fi
+
+tail -$NUM_TOPIC $TOPIC_DIR/tmp/output.csv.$NUM_TOPIC.summary.txt | cut -c 33- > tmp/topic_cluster.idx
+#tail -$NUM_TOPIC $TOPIC_DIR/tmp/output.csv.$NUM_TOPIC.summary.txt | cut -c 33- | nl -n ln -v 0 > tmp/topic_cluster.idx
