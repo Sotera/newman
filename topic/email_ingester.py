0b01e4d547ab1537f32333153305931ceb6340c5
Scott Haleen <scott.haleen@gmail.com>
2014-10-07 16:52:59 -0400

    removed non standard characters

--- topic/email_ingester.py
+++ topic/email_ingester.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 # +-----+-----+----------+----------+------------+------+----+----+----+-----+-------------+-----------+-----------+------------+---------+------+
 # |  0  |  1  |  2       |  3       |  4         |  5   |  6 | 7  | 8  |  9  |  10         |   11      |  12       |  13        |  14     |  15  |
@@ -11,12 +12,16 @@ def get_fields (ln):
     output = {}
     f = ln.split('\t')
     num_fields = len(f)
-    if (num_fields < 15):
+    if (num_fields < 14):
         output = None
         return output
 
+    if (num_fields == 15):
+        f.append('')
     output['id'] = f[0]
     output['subject'] = f[14]
     #body
-    output['msg'] = f[15].replace('[:newline:]',' ').replace('[', '').replace(']', '').replace('mailto:', '')
+    msg = f[15]
+    msg = re.sub(r'[^\x00-\x7F]',' ', msg)
+    output['msg'] = msg.replace('[:newline:]',' ').replace('[', '').replace(']', '').replace('mailto:', '')
     return output
