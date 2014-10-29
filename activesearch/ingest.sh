4e0682cd65bad77500054e94d82b66b97d22ecba
Scott Haleen <scott.haleen@gmail.com>
2014-10-29 17:55:29 -0400

    as update

--- activesearch/ingest.sh
+++ activesearch/ingest.sh
@@ -7,7 +7,6 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-
 source $1
 
 RUN_DIR=$(pwd)
@@ -19,4 +18,6 @@ printf "working dir $RUN_DIR\n"
 printf "active search dir $AS_DIR\n"
 printf "ingesting - $EMAIL_TARGET \n"
 
-perl import_database.pl -file=$RUN_DIR/demail/emails/$EMAIL_TARGET/output.csv -database=$AS_DATABASE -database_username=$AS_DATABASE_USERNAME -database_password=$AS_DATABASE_PASSWORD
+cd $AS_DIR 
+
+perl Database/import_database.pl -file=$RUN_DIR/demail/emails/$EMAIL_TARGET/output.csv -database=$AS_DATABASE -database_username=$AS_DATABASE_USERNAME -database_password=$AS_DATABASE_PASSWORD
