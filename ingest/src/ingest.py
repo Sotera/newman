176642a222de9025ef7308aaf960cddcc06bcd32
Scott Haleen <scott.haleen@gmail.com>
2015-01-30 18:03:59 -0500

    removed print statement

--- ingest/src/ingest.py
+++ ingest/src/ingest.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
                 continue
 		
             if references != '' and references.split()[0] != '':
-                print references
+                #print references
 		threadid = references.split()[0]
 
             tosize = len(toemail.split(';'))
