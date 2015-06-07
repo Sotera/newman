1a475f6fc680ed412d1b1be95c33de23221d851f
Scott Haleen <scott.haleen@gmail.com>
2015-06-06 22:56:40 -0400

    updated search page UI created new mediasearch service

--- demail/js/searchtool.js
+++ demail/js/searchtool.js
@@ -1,62 +1,72 @@
 $(function () {
-    $("#search-btn").click(function () {
-        var text = $("#txt_search").val();
-        var tbl_instagram = document.getElementById("ig");
-        var tbl_twitter = document.getElementById("tw");
-        var linkInstagram = "#!/search/instagram/";
-        var linkTwitter = "#!/search/twitter/";
 
-        var instagram = $.ajax({
-            url: 'media/search/instagram/' + text,
-            type: "GET",
-            data: function(un){ JSON.stringify(un) },
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
+  var do_search = function () {
+    var text = $("#txt_search").val();
+    var tbl_instagram = $("#ig>tbody");
+    var tbl_twitter = $("#tw>tbody");
+    var linkInstagram = "/#!/user/instagram/";
+    var linkTwitter = "/#!/user/twitter/";
 
-        });
-
-        var twitter = $.ajax({
-            url: 'media/search/twitter/' + text,
-            type: "GET",
-            data: function(un) { JSON.stringify(un) },
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        });
-
-        $.when(instagram, twitter)
-            .done(function (igResponse, tResponse) {
-                if (igResponse[0].results.length === 0) {
-                    console.log("No Instagram username found using keyword: " + text);
-                };
-                if (tResponse[0].results.length === 0) {
-                    console.log("No Twitter username found using keyword: " + text);
-                };
-                insertData(igResponse, tResponse);
-            })
-            .fail(function () {
-                console.log("Fail");
-            });
-
-        function insertData(instagram, twitter) {
-            var rowI = tbl_instagram.insertRow();
-            var cellI = rowI.insertCell();
-            var rowT = tbl_twitter.insertRow();
-            var cellT = rowT.insertCell();
-
-            for (var i = 0; i < instagram[0].results.length; i++) {
-                cellI.innerHTML = createIgHyperlink(instagram[0].results[i].name);
-            };
-
-            for (var i = 0; i < twitter[0].results.length; i++) {
-                cellT.innerHTML = createTwHyperlink(twitter[0].results[i].name);
-            };
-        }
-
-        function createIgHyperlink(username) {
-            return "<a href=" + linkInstagram + username + ">" + username + "</a>";
-        }
-        function createTwHyperlink(username) {
-            return "<a href=" + linkTwitter + username + ">" + username + "</a>";
-        }
+    var instagram = $.ajax({
+      url: 'mediasearch/user/instagram/' + text,
+      type: "GET",
+      data: function(un){ JSON.stringify(un) },
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
     });
+
+    var twitter = $.ajax({
+      url: 'mediasearch/user/twitter/' + text,
+      type: "GET",
+      data: function(un) { JSON.stringify(un) },
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+    });
+
+    $.when(instagram, twitter)
+      .done(function (igResponse, tResponse) {
+        tbl_instagram.empty();
+        tbl_twitter.empty();
+        if (igResponse[0].length === 0){
+          tbl_instagram.append($('<tr>').append($('<td>').html('No Users Found')))
+        }
+        if (tResponse[0].length === 0) {
+          tbl_twitter.append($('<tr>').append($('<td>').html('No Users Found')))
+        }
+
+        $.each(tResponse[0], function(i, twt_name){
+          tbl_twitter.append(
+            $('<tr>').append(
+              $('<td>').append(
+                $('<a>', 
+                  {"href" : createTwHyperlink(twt_name), 
+                   "text": twt_name}))))})
+
+          $.each(igResponse[0], function(i, ig_name){
+            tbl_instagram.append(
+              $('<tr>').append(
+                $('<td>').append(
+                  $('<a>', 
+                    {"href" : createIgHyperlink(ig_name), 
+                     "text": ig_name}))))});
+      })
+      .fail(function () {
+        console.log("Fail");
+      });
+
+    function  createIgHyperlink(username) {
+      return linkInstagram + username;
+    }
+    function createTwHyperlink(username) {
+      return linkTwitter + username;
+    }
+  }
+
+  $('#txt_search').keypress(function (e) {
+    if (e.which == 13) {
+      do_search();
+      return false;  
+    }});
+
+  $("#search-btn").click(function(){ do_search(); });
 });
---
+++ demail/mediasearch.py
@@ -0,0 +1,163 @@
+
+from newman.utils.functions import nth, rest, head, jsonGet
+
+from elasticsearch import Elasticsearch
+
+import tangelo
+import cherrypy
+import json
+import urllib
+import time
+import datetime
+import dateutil
+import dateutil.parser
+import itertools
+
+def groupBy(l, keyFn):
+    return [[k, list(g) ] for k, g in itertools.groupby(sorted(l, key=keyFn), key=keyFn)]
+
+def epochTimeStr(long_time):
+    try:
+        return datetime.datetime.utcfromtimestamp(long_time).strftime('%Y-%m-%dT%H:%M:%S')
+    except:
+        return ""
+
+def parseTwitterStr(sz):
+    try:
+        return dateutil.parser.parse(sz).strftime('%Y-%m-%dT%H:%M:%S')
+    except:
+        return ""
+
+url='10.1.92.76:9200'
+index_name = 'newman-media'
+es = Elasticsearch([url])
+
+index_map = {
+    "twitter" : "twitter",
+    "instagram" : "instagram-ny"
+}
+
+def igpost(j):
+    o={}
+    o['id']=jsonGet(["_id"],j)
+    o['created']=epochTimeStr(long(jsonGet(["_source", "post", "created_time"], j)))
+    o['content']=jsonGet(["_source", "post", "caption","text"], j, "")
+    o['content_size'] = len(o['content'])
+    comments = jsonGet(["_source", "post", "comments", "data"], j, [])
+    o['assoc_users']= [ jsonGet(["from", "username"], c, "") for c in comments]
+    return o
+
+def twtpost(j):
+    o={}
+    o['id']=jsonGet(["_id"],j)
+    o['created']=parseTwitterStr(jsonGet(["_source", "post", "created_at"], j,""))
+    o['content']=jsonGet(["_source", "post", "text"],j,"")
+    o['content_size'] = len(o['content'])
+    mentions = jsonGet(["_source", "post", "entities", "user_mentions"], j, [])
+    mentions_users = [jsonGet(["screen_name"],m,"") for m in mentions]
+    o['assoc_users']=list(set(mentions_users))
+    return o
+
+def extractInstagram(posts):
+    o = {}
+    o['js'] = {}
+    o['js']['post_count'] = len(posts)
+    o['posts']= [ igpost(p) for p in posts]
+    o['js']['min_post'] = min(o['posts'], key=lambda x: x['created'])
+    o['js']['max_post'] = max(o['posts'], key=lambda x: x['created'])
+    return o
+
+def extractTwitter(posts):
+    o = {}
+    o['js'] = {}
+    o['js']['result_count'] = len(posts)
+    o['posts']= [ twtpost(p) for p in posts]
+    o['js']['min_post'] = min(o['posts'], key=lambda x: x['created'])
+    o['js']['max_post'] = max(o['posts'], key=lambda x: x['created'])
+    return o
+
+def similar(account_source, username):
+    field = {"twitter": "twitter_id", "instagram" : "instagram_id" }.get(account_source)
+    res = es.search(index=index_name, 
+                    doc_type='similar', 
+                    size=5000, 
+                    body={"query": {"match": { field : username }}})
+    r = jsonGet(['hits','hits'], res, [])
+    return [ jsonGet(['_source'], item) for item in r ]
+
+def buildGraph(posts):
+    #o = { 'links' : [], 'nodes': [] } 
+    o={}
+    links = [(u, p['id'])
+             for p in posts
+             for u in p['assoc_users']]
+    
+    users = [{"type": "user", "name": u, "value": len(l) } for u, l in groupBy([l[0] for l in links], lambda x: x)]
+    posts = [{"type": "post", "name": p, "value": len(l) } for p, l in groupBy([l[1] for l in links], lambda x: x)]
+    nodes= users+posts
+    lookup_idx = {o['name']:i for i, o in enumerate(nodes)}
+    edges = [{'source': lookup_idx[l[0]], 'target': lookup_idx[l[1]], 'value': 1} for l in links]
+    #o['lookup'] = lookup_idx
+    o['nodes'] = nodes
+    o['links'] = edges
+    return o
+
+def search(which, user):
+    doc_type=index_map.get(which)
+    res = es.search(index=index_name, 
+                    doc_type=doc_type, 
+                    size=5000, 
+                    body={"query": {"match": { "username" : user }}})
+    return jsonGet(['hits', 'hits'], res, [])
+
+def instagram_search(*args):
+    instagram_id=urllib.unquote(nth(args, 0, ''))
+    esresults = search('instagram', instagram_id)
+    o = extractInstagram(esresults)
+    o['similar'] = similar('instagram', instagram_id)
+    o['graph'] = buildGraph(o['posts'])
+    tangelo.content_type("application/json")        
+    return o
+
+def twitter_search(*args):
+    twitter_id=urllib.unquote(nth(args, 0, ''))
+    esresults = search('twitter', twitter_id)
+    o = extractTwitter(esresults)
+    o['similar'] = similar('twitter', twitter_id)
+    o['graph'] = buildGraph(o['posts'])
+    tangelo.content_type("application/json")        
+    return o
+
+def user_search(*args):
+    doc_type=index_map.get(urllib.unquote(nth(args, 0)))
+    text=urllib.unquote(nth(args, 1, "")) + ".*"
+    res = es.search(index=index_name, 
+                    doc_type=doc_type, 
+                    size=0, 
+                    body={"fields": ["username"], 
+                          "query": { "regexp": { "username" : text }},
+                          "aggs" : { "users" : {"terms" : { "field" : "username" } } }})
+
+    tangelo.content_type("application/json")        
+    return [jsonGet(['key'], r) for r in jsonGet(['aggregations', 'users', 'buckets'], res, [])]
+
+get_actions = {
+    "instagram": instagram_search,
+    "twitter" : twitter_search,
+    "user": user_search
+}
+
+post_actions = {}
+
+def unknown(*args):
+    return tangelo.HTTPStatusCode(400, "invalid service call")
+
+@tangelo.restful
+def post(*pargs, **kwargs):
+    post_data = json.loads(cherrypy.request.body.read())
+    path = '.'.join(pargs)
+    return post_actions.get(path, unknown)(post_data)
+
+@tangelo.restful
+def get(action, *args, **kwargs):
+    return get_actions.get(action, unknown)(*args)
