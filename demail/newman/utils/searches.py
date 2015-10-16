80662a6e0bf9a6376bd94c3f78ae6cb1e52fc57e
Elliot Ickovic <elliot.ickovic@gmail.com>
2015-10-16 14:20:44 -0400

    pathed email graph

--- demail/newman/utils/searches.py
+++ demail/newman/utils/searches.py
@@ -1,43 +1,76 @@
-
 from elasticsearch import Elasticsearch
 import tangelo
 import json
 
+# contains a cache of all email_address.addr, email_address
+_email_addr_cache = None
+
 _row_fields = ["id","tos","senders","ccs","bccs","datetime","subject"]
 _graph_fields = ["community", "community_id", "addr", "received_count", "sent_count", "recepient.email_id", "sender.email_id"]
+_email_addr_cache_fields= ["community", "community_id", "addr", "received_count", "sent_count"]
+
 # Sort which will add sent + rcvd and sort most to top
 _sort_email_addrs_by_total={ "_script": { "script_file": "email_addr-sent-rcvd-sum", "lang": "groovy", "type": "number","order": "desc" }}
 _query_all = {"bool":{"must":[{"match_all":{}}]}}
 
-def count(index, start="2000-01-01", end="now"):
+
+def count(index, type="emails", start="2000-01-01", end="now"):
     es = Elasticsearch()
     # TODO apply filter to query not to body
     filter = {"range" : {"datetime" : { "gte": start, "lte": end }}}
     all_query = {"bool":{"must":[{"match_all":{}}]}}
-    count = es.count(index=index, doc_type="emails", body={"query" : all_query})
+    count = es.count(index=index, doc_type=type, body={"query" : all_query})
 
     return count["count"]
 
 
-def map_rows(doc):
+def _map_rows(doc):
     fields = doc["fields"]
     row = {}
     row["num"] =  fields["id"][0]
 
-    row["from"] = dict_default(fields,"senders", [""])[0]
-    row["to"] = reduce_address(dict_default(fields, "tos"))
-    row["cc"] = reduce_address(dict_default(fields,"ccs"))
-    row["bcc"] = reduce_address(dict_default(fields,"bccs"))
-    row["datetime"] = dict_default(fields,"datetime",[""])[0]
-    row["subject"] =  dict_default(fields,"subject",[""])[0]
+    row["from"] = fields.get("senders")[0]
+    row["to"] = ';'.join(fields.get("tos"))
+    row["cc"] = ';'.join(fields.get("ccs"))
+    row["bcc"] = ';'.join(fields.get("bccs"))
+    row["datetime"] = fields.get("datetime",[""])[0]
+    row["subject"] =  fields.get("subject",[""])[0]
     row["fromcolor"] =  1950
     row["attach"] =  ""
     row["bodysize"] =  0
     return row
 
-# def create_graph_from_email(index, email_addresses):
-#     query =  {"filtered" : { "filter" : {"bool" { "term" : { "productID" : email_addresses}}}}}
+def _map_emails(fields):
+    row = {}
+    row["num"] =  fields["id"][0]
 
+    row["from"] = fields.get("senders",[])[0]
+    row["to"] = fields.get("tos", [])
+    row["cc"] = fields.get("ccs", [])
+    row["bcc"] = fields.get("bccs", [])
+    row["datetime"] = fields.get("datetime",[""])[0]
+    row["subject"] =  fields.get("subject",[""])[0]
+    row["fromcolor"] =  1950
+    row["attach"] =  ""
+    row["bodysize"] =  0
+    return row
+
+def _map_emails_to_row(row):
+    row["to"] = ';'.join(row["to"])
+    row["cc"] = ';'.join(row["cc"])
+    row["bcc"] = ';'.join(row["bcc"])
+    return row
+
+
+def _map_node(email_addr, total_docs):
+    node={}
+    name = email_addr["addr"][0]
+    node["commumity"] =  email_addr["community"][0]
+    node["group"] =  email_addr["community_id"][0]
+    node["name"] = name
+    node["num"] =  email_addr["sent_count"][0] + email_addr["received_count"][0]
+    node["rank"] = (email_addr["sent_count"][0] + email_addr["received_count"][0]) / float(total_docs)
+    return node
 
 def create_graph(index, email_addresses):
     emailIndex= {}
@@ -99,21 +132,11 @@ def create_graph(index, email_addresses):
 
         del node["rcvr"]
         del node["sender"]
-    #
-    # text_file = open("edge.json", "w")
-    # text_file.write(json.dumps(edgeMap))
-    # text_file.close()
 
     for side_a, adjacent in edgeMap.iteritems():
         for side_b, num in adjacent.iteritems():
             edges.append({"source" : emailIndex[side_a],"target": emailIndex[side_b] ,"value": num})
     return {"nodes" : nodes, "links" : edges}
-
-def dict_default(dict, key, default=""):
-    return dict[key] if key in dict else default
-
-def reduce_address(addresses, tokenizer=";"):
-    return str(tokenizer).join(str(item) for item in addresses)
 
 # Get search all
 def _search_ranked_email_addrs(index, start, end, size):
@@ -132,101 +155,83 @@ def build_ranked_graph(*args, **kwargs):
     graph_results = create_graph(graph_results.get('hits').get('hits'))
     return {"graph":graph_results, "rows":[]}
 
+def _load_email_addr_cache(index):
+    es = Elasticsearch()
+
+    body={"query" : {"match_all" : {}}}
+
+    num = count(index,"email_address")
+    print num
+    addrs = es.search(index=index, doc_type="email_address", size=num, fields=_email_addr_cache_fields, body=body)
+    _email_addr_cache = {f["addr"][0]:_map_node(f,num) for f in [hit["fields"] for hit in addrs["hits"]["hits"]]}
+    print "done: ", num
+    return _email_addr_cache
+
+# This will generate the graph structure for a specific email address.  Will aply date filter and term query.
+def _create_graph_from_email(index, email_address, start, end, terms=[], size=2000):
+    term_query = {"match_all" : {}} if not terms else {"match" : {"_all" : " ".join(terms)}}
+
+    query_email_addr =  {"query":{"filtered" : {
+        "query" : term_query,
+        "filter" : {"bool":{
+            "should":[
+                {"term" : { "senders" : email_address}},
+                {"term" : { "tos" : email_address}},
+                {"term" : { "ccs" : email_address}},
+                {"term" : { "bccs" : email_address}}
+            ],
+            "must":[{"range" : {"datetime" : { "gte": start, "lte": end }}}]
+        }}}}}
+
+
+    es = Elasticsearch()
+    emails_resp = es.search(index=index, doc_type="emails", size=size, fields=_row_fields, body=query_email_addr)
+
+    emails = [_map_emails(hit["fields"])for hit in emails_resp["hits"]["hits"]]
+
+    nodes = []
+    edge_map = {}
+    addr_index = {}
+    for email in emails:
+        from_addr = email["from"]
+        if from_addr not in addr_index:
+            nodes.append(_email_addr_cache[from_addr])
+            addr_index[from_addr] = len(nodes)
+        for rcvr_addr in email["to"]+email["cc"]+email["bcc"]:
+            if rcvr_addr not in addr_index:
+                nodes.append(_email_addr_cache[rcvr_addr])
+                addr_index[rcvr_addr] = len(nodes)
+            #TODO reduce by key instead of mapping?  src->target and sum on value
+            edge_key = from_addr+"#"+rcvr_addr
+            if edge_key not in edge_map:
+                edge_map[edge_key] = {"source" : addr_index[from_addr],"target": addr_index[rcvr_addr],"value": 1}
+            else:
+                edge_map[edge_key]["value"]=edge_map[edge_key]["value"]+1
+
+    return {"graph":{"nodes":nodes, "links":edge_map.values()}, "rows": [_map_emails_to_row(email) for email in emails]}
+
 # GET /search/<query string>?index=<index name>&start=<start datetime>&end=<end datetime>
 # build a graph for a specific email address.  This will use a high performance mget operation
 # Rewrote this query to build through the community
+# args should be a list of terms to search for in any document field
 def get_graph_for_email_address(*args, **kwargs):
-    start = kwargs["start"]
-    end = kwargs["end"]
-    index = kwargs["index"]
-    email_addr = kwargs["email_addr"]
-
-    es = Elasticsearch()
-
-    query_email_addr = { "query" : { "filtered" : { "filter" : { "term" : { "addr" : email_addr}}}}}
-
-    parent_obj = es.search(index=index, doc_type="email_address", size=100, body=query_email_addr)
-
-    community = parent_obj["hits"]["hits"][0]["_source"]["community"]
-
-    local = es.search(index=index, doc_type="email_address", size=2000, body={"query":{"bool":{"must":[{"term": {"community":community}}]}}})
-    # for addrs in local["hits"]["hits"]:
-    print local
+    return _create_graph_from_email(kwargs["index"], kwargs["email_addr"], kwargs["start"], kwargs["end"], args, kwargs["size"])
 
 
-# build a graph for a specific email address.  This will use a high performance mget operation
-# THis was a first attempt and was not querying through the community
-def get_graph_for_email_address_old(index, email_addr):
-    es = Elasticsearch()
-
-    query_email_addr = { "query" : { "filtered" : { "filter" : { "term" : { "addr":email_addr}}}}}
-    graph_results = es.search(index=index, doc_type="email_address", body=query_email_addr)
-    # TODO
-    # graph_results = createGraph(graph_results)
-
-    email_ids = set([])
-    # TODO data time filtering on email_address sent / rcvd results
-    for item in graph_results["hits"]["hits"][0]["_source"]["sender"] + graph_results["hits"]["hits"][0]["_source"]["recepient"]:
-        email_ids.add(item["email_id"])
-
-    rows = get_rows(email_ids)
-
-    # get all email_address references
-    refs = set([])
-    for row in rows:
-        for field in ["tos", "senders", "ccs", "bccs"]:
-            if field in row["fields"] and row["fields"][field]:
-                refs.update(row["fields"][field])
-    print refs
-    #Query all the refs from email_addresses these will be all the graph.nodes
-    # links will be refs
-    #Generate the graph
-    email_addrs = {"ids" : list(refs)[:1000]}
-    email_addrs = es.mget(index=index, doc_type="email_address", fields=_graph_fields, body=email_addrs)["docs"]
-    graph_results = create_graph(email_addrs)
-
-    # convert rows to UI json format
-    rows = [map_rows(doc) for doc in rows]
-
-    return {"graph":graph_results, "rows":rows}
-
-
-# TODO for now assume that the list of ids is filtered based on date / time
-# get 1000 rows for given ids[]
-def get_rows(ids=[]):
+def _mget_rows(ids=[]):
     es = Elasticsearch()
 
     print len(ids)
-    row_body= {"ids" : list(ids)[:1000]}
+    row_body= {"ids" : list(ids)}
     row_results = es.mget(index="sample", doc_type="emails", fields=_row_fields, body=row_body)
     row_results = row_results["docs"]
 
     return row_results
 
-# deprecated
-def populate_rows(email_addr):
-    start="2000-01-01"
-    end="now"
-    es = Elasticsearch()
-
-    filter =  {"range" : {"datetime" : { "gte": start, "lte": end }}}
-
-    all_query = {"bool":{"must":[{"match_all":{}}]}}
-    row_fields = ["id","tos","senders","ccs","bccs","datetime","subject"]
-
-    row_body= {"fields": row_fields, "query": all_query, "filter":filter }
-
-    row_results = es.search(index="sample", doc_type="emails", size=2000, body=row_body)
-    row_results = row_results.get('hits').get('hits')
-    row_results = [map_rows(hit) for hit in row_results]
-    # print len(row_results)
-    # print json.dumps(row_results)
-    return row_results
-
-
 if __name__ == "__main__":
     # res = buildGraph()
-    res = get_graph_for_email_address("sample","tom.barry@myflorida.com")
+    _email_addr_cache = _load_email_addr_cache("sample")
+    res = _create_graph_from_email("sample","tom.barry@myflorida.com","2001","now", terms=["swamped"])
     text_file = open("/home/elliot/graph.json", "w")
     text_file.write(json.dumps(res))
     text_file.close()
