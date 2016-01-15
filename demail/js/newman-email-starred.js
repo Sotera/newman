71c019baee16a2f05a0f9e1b6a625b5cfdfd3f8b
Jakob Lee <simplexity.product@gmail.com>
2016-01-15 15:08:09 -0500

    fixed starred/export download

--- demail/js/newman-email-starred.js
+++ demail/js/newman-email-starred.js
@@ -168,26 +168,27 @@ var newman_email_starred_request_export = (function () {
     $.ajax({
       url: service_url,
       type: "GET",
-      contentType: "application/x-gzip",
+      headers:{'Content-Type':'application/x-gzip','X-Requested-With':'XMLHttpRequest'},
       dataType: "binary",
-      processData: false
-    }).done(function (response) {
-
+      processData: false,
+      success: function(response) {
+        window.location = service_url;
 
         console.log('response :\n' + JSON.stringify(response, null, 2));
 
-        $('#export_modal').modal('show');
-
-        //$('#export_download_link a').attr('href', response.file);
+        //TODO: download pop-up not working; needs re-work
+        $('#export_download_link a').attr('href', 'export.tar.gz');
         $('#export_link_spin').hide();
         $('#export_download_link').show();
-      })
-      .fail(function(response) {
 
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
         alert('Failed to download!');
 
         $('#export_modal').modal('hide');
-      });
+
+      }
+    });
 
   }
 
@@ -252,3 +253,52 @@ var newman_email_starred_request_all = (function () {
   }
 
 }());
+
+/**
+ *
+ * @description. jQuery ajax transport to handle binary data type requests.
+ *
+ */
+// use this transport for "binary" data type
+$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+  // check for conditions and support for blob / arraybuffer response type
+  if (window.FormData && ((options.dataType && (options.dataType == 'binary')) ||
+                          (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) ||
+                          (window.Blob && options.data instanceof Blob))))) {
+    return {
+      // create new XMLHttpRequest
+      send: function(headers, callback){
+        // setup all variables
+        var xhr = new XMLHttpRequest(),
+          url = options.url,
+          type = options.type,
+          async = options.async || true,
+        // blob or arraybuffer. Default is blob
+          dataType = options.responseType || "blob",
+          data = options.data || null,
+          username = options.username || null,
+          password = options.password || null;
+
+        xhr.addEventListener('load', function(){
+          var data = {};
+          data[options.dataType] = xhr.response;
+          // make callback and send data
+          callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+        });
+
+        xhr.open(type, url, async, username, password);
+
+        // setup custom headers
+        for (var i in headers ) {
+          xhr.setRequestHeader(i, headers[i] );
+        }
+
+        xhr.responseType = dataType;
+        xhr.send(data);
+      },
+      abort: function(){
+        jqXHR.abort();
+      }
+    };
+  }
+});
