d1c0c74115eb18af761bcddb62967066fcf8d6da
Jakob Lee <simplexity.product@gmail.com>
2017-03-16 00:43:06 -0400

    updated start/stop scripts to invoke map-tile server start/stop scripts

--- start.sh
+++ start.sh
@@ -4,3 +4,8 @@ echo "Starting supervisord -- Error is expected if newman supervisor is running.
 
 echo "Starting newman"
 ./flask/bin/supervisorctl -c conf/supervisord.conf start newman
+
+echo "Starting newman-tile-server"
+./bin/couchdb_stop.sh
+./bin/couchdb_start.sh
+
--- stop.sh
+++ stop.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 ./flask/bin/supervisorctl -c conf/supervisord.conf stop newman
+
+echo "Stopping newman-tile-server"
+./bin/couchdb_stop.sh
+
