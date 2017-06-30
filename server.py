845127333d0f1353d3d8149e46bd405d6cdb24b7
Jakob Lee <simplexity.product@gmail.com>
2017-06-30 18:06:41 -0400

    set default server start in non-secure mode (host='0.0.0.0') by default, to allow binding to any address

--- server.py
+++ server.py
@@ -5,5 +5,9 @@ from app import app
 # ref. http://stackoverflow.com/questions/25504149/why-does-running-the-flask-dev-server-run-itself-twice
 # app.run(port=5000, debug=True, host='0.0.0.0', use_reloader=False)
 
-app.run(host='127.0.0.1', port=5000, debug=True, use_evalex=False, use_reloader=False)
+# start server and allow binding to local host
+# app.run(host='127.0.0.1', port=5000, debug=True, use_evalex=False, use_reloader=False)
+
+# start server and allow binding to any address
+app.run(host='0.0.0.0', port=5000, debug=True, use_evalex=False, use_reloader=False)
 
