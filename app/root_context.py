2cdec852b150cbcc2a31c96ccb5c0d7b6e4ab183
jstnldrs <justin.lueders@gmail.com>
2018-10-02 13:21:19 -0500

    config for default user

--- app/root_context.py
+++ app/root_context.py
@@ -3,17 +3,16 @@ from flask import send_from_directory
 from flask import session, request
 
 
-app.secret_key = app.config["root_context"].get('newman_secret_key')
+app.secret_key = app.config["root_context"].get('secret_key')
 
 
 def validate_user():
-    username = 'elasticsearch' if 'username' not in request.headers else request.headers['username']
+    username = app.config["root_context"].get('default_user') if 'username' not in request.headers else request.headers['username']
     amino_user = None if 'aminoUser' not in session else session['aminoUser']
 
     if username is not amino_user or amino_user is None:
         session['amino_user'] = username
         session['amino_token'] = ''
-
 
 
 @app.route('/')
